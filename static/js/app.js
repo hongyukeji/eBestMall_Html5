d695dd33b98c192a25037985c8298e4bed3dbd1c
Shadow <admin@hongyuvip.com>
2017-02-22 16:18:59 +0800

    eBestMall_Html5

--- static/js/app.js
+++ static/js/app.js
@@ -3,23 +3,154 @@ $(function () {
     // console.log();
 });
 
-/* 首页-滚动事件 */
+/* VueJs */
 $(function () {
+    var vm = new Vue({
+        el: "#searchBar",
+        data: {
+            title: "Hello Vue"
+        },
+        mounted: function () {
+            // console.log('Title is: ' + this.title);
+            this.edit();
+        },
+        methods: {
+            edit: function () {
+                this.title = "Hello eBestMall";
+            }
+        }
+    });
+});
 
+
+$(document).ready(function(){
+    clickEvent();    // 全局单击事件
+    sideBar();    // 全局-侧边栏
+    indexScroll();    // 首页-滚动事件
+    bannerSlider();    // 首页-轮播图
+    noticeTab();    // 首页-公告Tab选项卡
+    secKill();    // 首页-秒杀
+    storeysTab();   // 首页-楼层Tab选项卡
+});
+
+/* 全局-单击事件-函数 */
+function clickEvent() {
+
+    /* 全局页面鼠标点击事件 */
+    $(document).click(function whichButton(event) {
+        var btnNum = event.button;
+        if (btnNum == 0) {
+            /*鼠标左键被点击*/
+            sideBarClose(); // 关闭侧边栏
+            $('.header-search-record').css('display', 'none');  // 关闭搜索下拉框
+        }
+        /*
+         if (btnNum == 2) {
+         // console.log("鼠标右键被点击！");
+         }
+         else if(btnNum==0){
+         // console.log("鼠标左键被点击！");
+         } else if (btnNum == 1) {
+         // console.log("鼠标中键被点击！");
+         } else {
+         // console.log("您点击了" + btnNum + "号键，我不能确定它的名称。");
+         }
+         */
+    });
+
+    /* 阻止侧边栏冒泡事件 */
+    $('#sideBar').click(function (event) {
+        event.stopPropagation();
+    });
+
+    /* 阻止搜索下拉框冒泡事件 */
+    $('.header-search').click(function (event) {
+        event.stopPropagation();
+    });
+
+    /* 关闭首页顶部广告 */
+    $('.close-event').on('click', function () {
+        $(this).parent().parent().fadeOut(588);
+        /* $(this).parent().next().find().addClass("hidden").removeClass("hidden");  // @父辈 parent() @同辈 next()  @儿子 find() */
+    });
+
+    /* Top返回顶部按钮被单击事件 */
+    $('.returnTop').click(function () {
+        returnTop();
+    });
+}
+
+/* 全局-侧边栏-函数 */
+function sideBar() {
+    /* 鼠标滚动监听事件 */
+    $(window).on('scroll', function () {
+        /* 侧边栏-返回顶部按钮 显示/隐藏事件 */
+        var returnTop = $('.returnTop');
+        if ($(window).scrollTop() > $(window).height()) {
+            returnTop.fadeIn();
+        } else {
+            returnTop.fadeOut();
+        }
+    });
+    $('.item-btn').click(function () {
+        var itemAll = $('.item');
+        var thisNow = $(this).parent(itemAll);
+        var sideBarRight = $('#sideBar').css("right");
+        var itemBody = $(this).parent(itemAll).find('.item-body').css("right");
+
+        if (sideBarRight == "0px" && itemBody == "-270px") {
+            sideBarOpen(thisNow);
+        } else if (sideBarRight == "270px" && itemBody == "0px") {
+            sideBarClose();
+        } else if (sideBarRight == "270px" && itemBody == "-270px") {
+            sideBarChange(thisNow, itemAll);
+        }
+    });
+    function sideBarOpen(thisNow) {
+        var sideBar = $('#sideBar');
+        var sideBarOpen = "270px";
+        var itemBg = "#C40000";
+        sideBar.animate({"right": sideBarOpen}, "fast");
+        thisNow.css({"background-color": itemBg}).find('.item-body').animate({"right": "0"}, "fast");
+    }
+
+    function sideBarClose() {
+        var sideBar = $('#sideBar');
+        var sideBarClose = "0px";
+        var itemBodyClose = "-270px";
+        sideBar.animate({"right": sideBarClose}, "fast");
+        $('.item').css({"background-color": ""}).find('.item-body').animate({"right": itemBodyClose}, "fast");
+    }
+
+    function sideBarChange(thisNow, thisAll) {
+        var itemBodyOpen = "0px";
+        var itemBodyClose = "-270px";
+        var itemBg = "#C40000";
+        thisAll.css({"background-color": ""}).find('.item-body').animate({"right": itemBodyClose}, "fast");
+        thisNow.css({"background-color": itemBg}).find('.item-body').animate({"right": itemBodyOpen}, "fast");
+    }
+
+}
+
+/* 全局-返回顶部-函数 */
+function returnTop() {
+    $('html,body').animate({scrollTop: 0}, 588);
+}
+
+
+/* 首页-滚动事件-函数 */
+function indexScroll() {
     /* 鼠标滚动监听事件 */
     $(window).on('scroll', function () {
 
         /* console.log($(window).scrollTop()); */
 
         /* 首页顶部搜索框-返回顶部按钮 显示/隐藏事件 */
-        var returnTop = $('.returnTop');
         var searchBar = $('#searchBar');
         if ($(window).scrollTop() > $(window).height()) {
             searchBar.fadeIn();
-            returnTop.fadeIn();
         } else {
             searchBar.fadeOut();
-            returnTop.fadeOut();
         }
 
         /* 首页-楼层导航-滚动 显示/隐藏/焦点事件 */
@@ -61,107 +192,10 @@ $(function () {
     $('.content-storey-nav .returnTop').on('click', function () {
         $(this).siblings('.item').removeClass('active');
     })
+}
 
-});
-
-/* 关闭首页顶部广告 */
-$(function () {
-    $('.close-event').on('click', function () {
-        $(this).parent().parent().fadeOut(588);
-        /* $(this).parent().next().find().addClass("hidden").removeClass("hidden");  // @父辈 parent() @同辈 next()  @儿子 find() */
-    });
-});
-
-/* 侧边栏 */
-$(function () {
-    /* 页面鼠标点击事件 */
-    $(document).click(function whichButton(event) {
-        var btnNum = event.button;
-        if (btnNum == 0) {
-            /*鼠标左键被点击*/
-
-            // 关闭侧边栏
-            sideBarClose();
-
-            // 关闭搜索下拉框
-            $('.header-search-record').css('display', 'none');
-        }
-        /*
-         if (btnNum == 2) {
-         // console.log("鼠标右键被点击！");
-         }
-         else if(btnNum==0){
-         // console.log("鼠标左键被点击！");
-         } else if (btnNum == 1) {
-         // console.log("鼠标中键被点击！");
-         } else {
-         // console.log("您点击了" + btnNum + "号键，我不能确定它的名称。");
-         }
-         */
-    });
-
-    /* 阻止侧边栏冒泡事件 */
-    $('#sideBar').click(function (event) {
-        event.stopPropagation();
-    });
-
-    /* 阻止搜索下拉框冒泡事件 */
-    $('.header-search').click(function (event) {
-        event.stopPropagation();
-    });
-
-    /* Top返回顶部按钮被单击事件 */
-    $('.returnTop').click(function () {
-        returnTop();
-    });
-
-    /* 返回顶部函数 */
-    function returnTop() {
-        $('html,body').animate({scrollTop: 0}, 588);
-    }
-
-    $('.item-btn').click(function () {
-        var itemAll = $('.item');
-        var thisNow = $(this).parent(itemAll);
-        var sideBarRight = $('#sideBar').css("right");
-        var itemBody = $(this).parent(itemAll).find('.item-body').css("right");
-
-        if (sideBarRight == "0px" && itemBody == "-270px") {
-            sideBarOpen(thisNow);
-        } else if (sideBarRight == "270px" && itemBody == "0px") {
-            sideBarClose();
-        } else if (sideBarRight == "270px" && itemBody == "-270px") {
-            sideBarChange(thisNow, itemAll);
-        }
-    });
-    function sideBarOpen(thisNow) {
-        var sideBar = $('#sideBar');
-        var sideBarOpen = "270px";
-        var itemBg = "#C40000";
-        sideBar.animate({"right": sideBarOpen}, "fast");
-        thisNow.css({"background-color": itemBg}).find('.item-body').animate({"right": "0"}, "fast");
-    }
-
-    function sideBarClose() {
-        var sideBar = $('#sideBar');
-        var sideBarClose = "0px";
-        var itemBodyClose = "-270px";
-        sideBar.animate({"right": sideBarClose}, "fast");
-        $('.item').css({"background-color": ""}).find('.item-body').animate({"right": itemBodyClose}, "fast");
-    }
-
-    function sideBarChange(thisNow, thisAll) {
-        var itemBodyOpen = "0px";
-        var itemBodyClose = "-270px";
-        var itemBg = "#C40000";
-        thisAll.css({"background-color": ""}).find('.item-body').animate({"right": itemBodyClose}, "fast");
-        thisNow.css({"background-color": itemBg}).find('.item-body').animate({"right": itemBodyOpen}, "fast");
-    }
-
-});
-
-/* 首页-轮播图 */
-$(function () {
+/* 首页-轮播图-函数 */
+function bannerSlider() {
     var bannerSlider = new Slider($('#sliderBar'), {
         time: 5000,
         delay: 400,
@@ -327,59 +361,10 @@ $(function () {
             }
         }
     }
-});
+}
 
-/* 首页-公告Tab选项卡 */
-$(function () {
-    tab('.content-focus-right-middle-tab-title li', '.content-focus-right-middle-tab-body ul');
-    function tab(tabTitles, tabContents) {
-        var tabTitle = $(tabTitles);
-        var tabContent = $(tabContents);
-        tabTitle.on('mouseover', function () {
-            if (tabTitle.length != tabContent.length) {
-                return false;
-            }
-            var index = tabTitle.index(this);
-            tabTitle.eq(index).find('a').addClass('active').parent().siblings().find('a').removeClass('active');
-            tabContent.eq(index).show().siblings().hide();
-        });
-    }
-});
-
-/* 首页-秒杀 */
-$(function () {
-    var tabBar = $('.content-sec-kill-body-list-wrapper ul');
-    var tabPrev = $('.content-sec-kill-body-list-wrapper-navigate-prev');
-    var tabNext = $('.content-sec-kill-body-list-wrapper-navigate-next');
-    var tabBarWidth = tabBar.width();
-    var tabPage = 1;
-    var tabPageNum = 5;
-    var tabBarNum = tabBar.find(".item").length;
-    var tabPageCount = Math.ceil(tabBarNum / tabPageNum);
-
-    tabNext.click(function () {
-        if (tabPage == tabPageCount) {
-            tabBar.animate({left: '0px'}, 'slow');
-            tabPage = 1;
-        } else {
-            tabBar.animate({left: '-=' + tabBarWidth}, 'slow');
-            tabPage++;
-        }
-    });
-    tabPrev.click(function () {
-        if (tabBar.css('left') == '0px' || tabBar.css('left') == 'auto') {
-            var _tabPageNum = tabPageNum - 1;
-            tabBar.animate({left: '-' + _tabPageNum * tabBarWidth}, 'slow');
-            tabPage = tabPageCount;
-        } else {
-            tabBar.animate({left: '+=' + tabBarWidth}, 'slow');
-            tabPage--;
-        }
-    });
-});
-
-/* 首页-楼层Tab选项卡 */
-$(function () {
+/* 首页-楼层Tab选项卡-函数 */
+function storeysTab() {
     storeyTab(".storey-tab-one-left");
     storeyTab(".storey-tab-one-right");
 
@@ -438,23 +423,51 @@ $(function () {
             }
         });
     }
-});
+}
 
-/* VueJs */
-$(function () {
-    var vm = new Vue({
-        el: "#searchBar",
-        data: {
-            title: "Hello Vue"
-        },
-        mounted: function () {
-            // console.log('Title is: ' + this.title);
-            this.edit();
-        },
-        methods: {
-            edit: function () {
-                this.title = "Hello eBestMall";
-            }
+/* 首页-秒杀-函数 */
+function secKill() {
+    var tabBar = $('.content-sec-kill-body-list-wrapper ul');
+    var tabPrev = $('.content-sec-kill-body-list-wrapper-navigate-prev');
+    var tabNext = $('.content-sec-kill-body-list-wrapper-navigate-next');
+    var tabBarWidth = tabBar.width();
+    var tabPage = 1;
+    var tabPageNum = 5;
+    var tabBarNum = tabBar.find(".item").length;
+    var tabPageCount = Math.ceil(tabBarNum / tabPageNum);
+
+    tabNext.click(function () {
+        if (tabPage == tabPageCount) {
+            tabBar.animate({left: '0px'}, 'slow');
+            tabPage = 1;
+        } else {
+            tabBar.animate({left: '-=' + tabBarWidth}, 'slow');
+            tabPage++;
         }
     });
-});
+    tabPrev.click(function () {
+        if (tabBar.css('left') == '0px' || tabBar.css('left') == 'auto') {
+            var _tabPageNum = tabPageNum - 1;
+            tabBar.animate({left: '-' + _tabPageNum * tabBarWidth}, 'slow');
+            tabPage = tabPageCount;
+        } else {
+            tabBar.animate({left: '+=' + tabBarWidth}, 'slow');
+            tabPage--;
+        }
+    });
+}
+
+/* 首页-公告Tab选项卡-函数 */
+function noticeTab() {
+    var tabTitle = $('.content-focus-right-middle-tab-title li');
+    var tabContent = $('.content-focus-right-middle-tab-body ul');
+    tabTitle.on('mouseover', function () {
+        if (tabTitle.length != tabContent.length) {
+            return false;
+        }
+        var index = tabTitle.index(this);
+        tabTitle.eq(index).find('a').addClass('active').parent().siblings().find('a').removeClass('active');
+        tabContent.eq(index).show().siblings().hide();
+    });
+}
+
