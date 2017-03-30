bc4fdb0b64c5de6d1e3f43c80d6e7b3b331b9ceb
Shadow <admin@hongyuvip.com>
2017-03-30 12:44:28 +0800

    eBestMall_Html5

---
+++ web/EbmMobileAsset.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ebestmall\web;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class EbmMobileAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/hongyukeji/ebestmall-html/mobile/static';
+    public $css = [
+        'css/app.css',
+    ];
+    public $js = [
+        'js/jquery.min.js',
+        'js/app.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+    ];
+}
