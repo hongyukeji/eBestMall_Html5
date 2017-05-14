322b00029c31d5717aa13f78dd7ccbb0a4211ca1
Shadow <admin@hongyuvip.com>
2017-05-14 16:02:37 +0800

    eBestMall_Html5

--- example-views/yiisoft/yii2-app/layouts/head.php
+++
@@ -1,9 +0,0 @@
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= \yii\helpers\Html::csrfMetaTags() ?>
-    <title><?= \yii\helpers\Html::encode($this->title) ?></title>
-    <link rel="shortcut icon" href="<?= Yii::$app->request->hostInfo ?>/favicon.ico" type="image/x-icon">
-    <?php $this->head() ?>
-</head>
