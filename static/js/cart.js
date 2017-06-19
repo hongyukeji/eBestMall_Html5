c0e72ff7d85942bde50685c4c28df62603a7936d
Shadow <admin@hongyuvip.com>
2017-06-19 20:52:16 +0800

    eBestMall_Html5

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,21 +1,56 @@
-/**
- * Created by Shadow on 2017/6/15.
- */
-$(document).ready(function () {
-});
-
-var vm = new Vue({
+new Vue({
     el: "#content",
     data: {
-        title:"eBestMall"
+        totalMoney: 0,
+        storeList: []
     },
     filters: {
-
     },
     mounted: function () {
-
+        this.$nextTick(function () {
+            this.cartView();
+        });
     },
     methods: {
-
+        cartView: function () {
+            var _this = this;
+            this.$http.get("data/storeData.json", {"id": 1}).then(function (res) {
+                _this.storeList = res.body.result.storeList;
+            });
+        },
+        changeNumber: function (goods) {
+            if (isNaN(goods.goodsNumber)){
+                goods.goodsNumber = 1;
+            }
+            if (goods.goodsNumber > 9999) {
+                goods.goodsNumber = 9999;
+            }
+            if (goods.goodsNumber <= 0) {
+                goods.goodsNumber = 1;
+            }
+            console.log(goods.goodsNumber);
+            console.log('此处执行ajax更新购物车列表中商品数量');
+        },
+        operationNumber: function (goods, way) {
+            if (way > 0) {
+                goods.goodsNumber++;
+            } else {
+                goods.goodsNumber--;
+            }
+            this.changeNumber(goods);
+        },
+        selectedGoods: function (item) {
+            if (typeof item.checked == 'undefined') {
+                Vue.set(item, "checked", true);
+            } else {
+                item.checked = !item.checked;
+            }
+        },
+        calcTotalPrice: function () {
+            var _this = this;
+        }
     }
+});
+Vue.filter("money", function (value) {
+    return value.toFixed(2);
 });
